0ade1a7bfe291dbe7d37bb1c7d09662f63c77797
Riley Miranda <mirandar1@udayton.edu>
2022-03-07 20:25:09 -0500

    started working on chatclient

---
+++ assignment1/ChatClient.js
@@ -0,0 +1,53 @@
+var net = require('net');
+ 
+if(process.argv.length != 4){
+	console.log("Usage: node %s <host> <port>", process.argv[1]);
+	process.exit(1);	
+}
+
+var host=process.argv[2];
+var port=process.argv[3];
+
+if(host.length >253 || port.length >5 ){
+	console.log("Invalid host or port. Try again!\nUsage: node %s <port>", process.argv[1]);
+	process.exit(1);	
+}
+
+var client = new net.Socket();
+console.log("ChatClient.js developed by Riley Miranda, SecAD");
+console.log("Connecting to: %s:%s", host, port);
+
+client.connect(port,host, connected);
+
+function connected(){
+	console.log("Connected to: %s:%s", client.remoteAddress, client.remotePort);
+
+	client.on("data", data => {
+		console.log("Received data: " + data);
+	});
+
+	client.on("error", function(err){
+		console.log("Error");
+		process.exit(2);
+	});
+	client.on("close", function(data){
+		console.log("Connection has been disconnected");
+		process.exit(3);
+	});
+
+	const keyboard = require('readline').createInterface({
+		input: process.stdin,
+		output: process.stout
+	});
+	keyboard.on('line', (input) => {
+		console.log(`You typed: ${input}`);
+		//Some code here to handle input
+		if(input === '.exit'){
+			client.destroy();
+			console.log('disconnected!');
+			process.exit();
+		} else {
+		client.write(input);
+		}
+	});
+}
