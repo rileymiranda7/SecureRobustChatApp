199b66f3ad818695762cb2dda9afadea2580e783
seed <mirandar1@udayton.edu>
2022-03-22 10:25:37 -0400

    form index php

---
+++ labs/lab5/form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Login page - SecAD</title>
+</head>
+<body>
+      	<h1>A Simple login form, SecAD</h1>
+
+<?php
+  //some code here
+  echo "Current time: " . date("Y-m-d h:i:sa")
+?>
+          <form action="index.php" method="POST" class="form login">
+                Username:<input type="text" class="text_field" name="username" /> <br>
+                Password: <input type="password" class="text_field" name="password" /> <br>
+                <button class="button" type="submit">
+                  Login
+                </button>
+          </form>
+
+</body>
+</html>
+
