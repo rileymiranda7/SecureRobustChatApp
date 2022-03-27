6120ef63ef7bf99200f9ded5d853aac274a7e24b
Riley Miranda <mirandar1@udayton.edu>
2022-03-27 18:10:31 -0400

    cleaned up some output

--- assignment1/ChatServer-v.0.0.go
+++ assignment1/ChatServer-v.0.0.go
@@ -1,4 +1,4 @@
-/* Simple EchoServer in GoLang by Phu Phung, customized
+/* ChatServer
 by Riley Miranda for SecAD*/
 package main
 
@@ -25,18 +25,9 @@ type User struct {
 	Key        string
 }
 
-//loginJson := `{"username": "bob", "password": "1234" }`
-//var myLogin Login //var login is type Login
-//json.Unmarshal([]byte(loginJson), &myLogin)
-// Now can access username and password using dot notation:
-// myLogin.username, myLogin.password
-
 var allLoggedIn_conns = make(map[net.Conn]interface{})
 var newclient = make(chan net.Conn)
 var lostclient = make(chan net.Conn)
-var loggedInClient = make(chan net.Conn)
-
-//var clientAuthenticated = make(chan net.Conn)
 
 func main() {
 	if len(os.Args) != 2 {
@@ -45,12 +36,12 @@ func main() {
 	}
 	port := os.Args[1]
 	if len(port) > 5 {
-		fmt.Println("Invalid port value. Try again!")
+		fmt.Println(">>Invalid port value. Try again!")
 		os.Exit(1)
 	}
 	server, err := net.Listen("tcp", ":"+port)
 	if err != nil {
-		fmt.Printf("Cannot listen on port '" + port + "'!\n")
+		fmt.Printf(">>Cannot listen on port '" + port + "'!\n")
 		os.Exit(2)
 	}
 	fmt.Println("ChatServer in GoLang developed by Riley Miranda")
@@ -58,23 +49,20 @@ func main() {
 	go func() {
 		for {
 			client_conn, _ := server.Accept()
-			fmt.Println("new client accepted")
 			newclient <- client_conn
 		}
 	}()
 	for {
 		select {
 		case client_conn := <-newclient:
-			fmt.Println("@case newclient")
+			fmt.Println("Debug>@case newclient")
 			loginSuccessful, username := login(client_conn)
 			if loginSuccessful {
 				var justLoggedInUser = User{Username: username, isLoggedIn: true}
 				allLoggedIn_conns[client_conn] = justLoggedInUser
-				//allLoggedIn_conns[client_conn] = client_conn.RemoteAddr().String()
 				welcomemessage := fmt.Sprintf("A new client '%s' connected!\n# of connected clients: %d\n",
 					justLoggedInUser.Username, len(allLoggedIn_conns))
 				fmt.Println(welcomemessage)
-				//broadcasting
 				sendtoAll([]byte(welcomemessage))
 				go client_goroutine(client_conn)
 			}
@@ -91,12 +79,10 @@ func main() {
 			}
 			fmt.Printf("%s\n", userlist)
 			sendtoAll([]byte(byemessage))
-			//client_conn.Close()
 		}
 	}
 }
 func client_goroutine(client_conn net.Conn) {
-	//var buffer [BUFFERSIZE]byte
 	var username = allLoggedIn_conns[client_conn].(User).Username
 	for {
 		menu := fmt.Sprintf("Type the number of the operation you would like to perform:\n" +
@@ -114,7 +100,7 @@ func client_goroutine(client_conn net.Conn) {
 		case "1":
 			sendUserList(client_conn)
 		case "2":
-			sendto([]byte("Type message:"), client_conn)
+			sendto([]byte("Type message to send to all:"), client_conn)
 			var input, read_err = readInput(client_conn)
 			if read_err != nil {
 				break
@@ -140,7 +126,7 @@ func client_goroutine(client_conn net.Conn) {
 		case "help":
 			continue
 		default:
-			sendto([]byte("Invalid Option\n"), client_conn)
+			sendto([]byte(">>Invalid Option\n"), client_conn)
 			continue
 		}
 	}
@@ -148,7 +134,6 @@ func client_goroutine(client_conn net.Conn) {
 func sendtoAll(data []byte) {
 	fmt.Println("@sendtoAll")
 	for client_conn, _ := range allLoggedIn_conns {
-
 		_, write_err := client_conn.Write(data)
 		if write_err != nil {
 			continue //move on next iteration
@@ -174,8 +159,7 @@ func sendUserList(client_conn net.Conn) {
 	for _, username := range userlistSlice {
 		userlist += username + ", "
 	}
-
-	sendto([]byte(userlist+"\n"), client_conn)
+	sendto([]byte(userlist+"\n\n"), client_conn)
 }
 func removeDuplicateStrings(slice []string) []string {
 	strmap := make(map[string]bool) // initialize map where key will be values of str slice
@@ -198,26 +182,23 @@ func userIsOnline(client_conn net.Conn, user string) bool {
 	if messageWasSent {
 		return true
 	} else {
-		sendto([]byte("Selected user is not online!\n"), client_conn)
+		sendto([]byte(">>Selected user is not online!\n\n"), client_conn)
 		return false
 	}
 }
 func sendPrivateM(receiver string, sender string, message string) {
 	for client_conn, _ := range allLoggedIn_conns {
 		if allLoggedIn_conns[client_conn].(User).Username == receiver {
-			sendto([]byte("[private message from "+sender+"]: "+message+"\n"), client_conn)
+			sendto([]byte("[private message from "+sender+"]: "+message+"\n\n"), client_conn)
 		}
 	}
 }
-
 func readInput(client_conn net.Conn) (string, error) {
-	//fmt.Println("@readInput()")
 	var buffer [BUFFERSIZE]byte
 	byte_received, read_err := client_conn.Read(buffer[0:])
-	//fmt.Printf("Read input of size %d\n", byte_received)
-	if read_err != nil /*|| byte_received < 3*/ {
+	if read_err != nil {
 		fmt.Println("@readInput(): Error in receiving or empty input...")
-		lostclient <- client_conn
+		lostclient <- client_conn // client disconnects
 		return "error", read_err
 	}
 	return string(buffer[:byte_received]), nil
@@ -225,11 +206,9 @@ func readInput(client_conn net.Conn) (string, error) {
 
 /**
 Get login data and call checklogin()
-If checklogin() returns true send client to authenticated channel to put on list
-Else send error message back to client and call login() again
+Returns true or calls login again
 */
 func login(client_conn net.Conn) (bool, string) {
-	//fmt.Println("@login()")
 	var loginJSONstring, _ = readInput(client_conn)
 	fmt.Printf("Received login JSON: %s\n", loginJSONstring)
 	loginSuccessful, message := checklogin(loginJSONstring, client_conn)
@@ -245,29 +224,24 @@ Parse the username and password
 Call checkaccount() to see if valid
 */
 func checklogin(loginJSONstring string, client_conn net.Conn) (bool, string) {
-	//fmt.Println("@checklogin()")
 	var account Account
 	err := json.Unmarshal([]byte(loginJSONstring), &account)
-	if err != nil /*|| account.username == "" || account.password == ""*/ {
+	if err != nil {
 		fmt.Printf("JSON parsing error: %s\n", err)
 		return false, "error"
 	}
-	//fmt.Printf("account.username: %s\naccount.password: %s\n", account.Username, account.Password)
 	return checkaccount(account, client_conn)
 }
 
 /**
-Compare username/password with database
+Check database and either return true or retry login
 */
 func checkaccount(account Account, client_conn net.Conn) (bool, string) {
-	//fmt.Println("@checkaccount()")
 	if (account.Username == "riley" && account.Password == "12345") ||
 		(account.Username == "user0" && account.Password == "67890") ||
 		(account.Username == "user00" && account.Password == "123456789") {
-		//fmt.Println("@checkaccount() -> username and password received")
 		return true, account.Username
 	} else {
-		//sendto([]byte("Invalid Username or Password...\n"), client_conn)
 		fmt.Printf("Login <%s, %s> failed.\n", account.Username, account.Password)
 		sendto([]byte("Login failed"), client_conn)
 		return login(client_conn)
