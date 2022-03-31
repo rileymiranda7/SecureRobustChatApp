8075d7dc038795fc1ae58d15d4a786388d88defb
seed <mirandar1@udayton.edu>
2022-03-30 21:37:32 -0400

    lab5

--- labs/lab5/index.php
+++ labs/lab5/index.php
@@ -1,53 +1,33 @@
 <?php
-	session_start();    
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+	session_start();   
+  $mysqli = new mysqli('localhost','mirandar1','1234','secad');
+      if($mysqli->connect_errno){
+        printf("Database connection failed: %s\n", $mysqli->connect_error);
+        exit();
+      } 
 	if (securechecklogin($_POST["username"],$_POST["password"])) {
 ?>
-	<h2> Welcome <?php echo $_POST['username']; ?> !</h2>
+	<h2> Welcome <?php echo htmlentities($_POST['username']); ?> !</h2>
 <?php		
 	}else{
 		echo "<script>alert('Invalid username/password');</script>";
 		die();
 	}
-	/*function checklogin($username, $password) {
-		$account = array("admin","1234");
-		if (($username== $account[0]) and ($password == $account[1])) 
-		  return TRUE;
-		else return FALSE;
-  	}*/
-  	function checklogin_sql($username, $password) {
-  		$mysqli = new mysqli('localhost',
-  								'mirandar1', //Database username
-  								'Peter6696', //Database password
-  								'secad'); // Name of database
-  		if($mysqli->connect_errno){
-  			printf("Database connection failed: %s\n", $mysqli->connect_error);
-  			exit();
-  		}
-  		$sql = "SELECT * FROM users WHERE username='" . $username. "' "; // no input validation
-  		$sql = $sql . "AND password = password('" . $password . "')"; // no input validation
-  		echo "DEBUG>sql= $sql";
-  		return TRUE;
-		/*$account = array("admin","1234");
-		if (($username== $account[0]) and ($password == $account[1])) 
-		  return TRUE;
-		else return FALSE;*/
-  	}
+
   	function securechecklogin($username, $password) {
-  		$mysqli = new mysqli('localhost',
-  								'mirandar1', //Database username
-  								'Peter6696', //Database password
-  								'secad'); // Name of database
-  		if($mysqli->connect_errno){
-  			printf("Database connection failed: %s\n", $mysqli->connect_error);
-  			exit();
-  		}
-  		$sql = "SELECT * FROM users WHERE username='" . $username. "' "; // no input validation
-  		$sql = $sql . "AND password = password('" . $password . "')"; // no input validation
-  		echo "DEBUG>sql= $sql";
-  		return TRUE;
-		/*$account = array("admin","1234");
-		if (($username== $account[0]) and ($password == $account[1])) 
-		  return TRUE;
-		else return FALSE;*/
+      global $mysqli;
+  		$prepared_sql = "SELECT * FROM users WHERE username= ? " .
+                      " AND password=password(?);";
+  		if(!$stmt = $mysqli->prepare($prepared_sql))
+        echo "Prepared statement error";
+      $stmt->bind_param("ss", $username,$password);
+      if(!$stmt->execute()) echo "Execute error";
+      if(!$stmt->store_result()) echo "Store result error";
+      $result = $stmt;
+      if($result->num_rows ==1)
+        return TRUE;
+      return FALSE;
   	}
 ?>
